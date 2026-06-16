2f1553d138e2d4b70f9570965e44268829cb7df9
Salvador Caldarella <s.caldarella82@gmail.com>
2026-06-16 16:27:49 -0300

    002 1

--- TareasLaborales/002/Program.cs
+++ TareasLaborales/002/Program.cs
@@ -1,24 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _002
+namespace PiedraPapelTijera
 {
     internal class Program
     {
-        /*
-         * Crea un programa que calcule quien gana más partidas al piedra,
-         * papel, tijera.
-         * - El resultado puede ser: "Player 1", "Player 2", "Tie" (empate)
-         * - La función recibe un listado que contiene pares, representando cada jugada.
-         * - El par puede contener combinaciones de "R" (piedra), "P" (papel)
-         *   o "S" (tijera).
-         * - Ejemplo. Entrada: [("R","S"), ("S","R"), ("P","S")]. Resultado: "Player 2".
-         */
         static void Main(string[] args)
         {
+            List<string> jugadasPlayer1 = new List<string>();
+            List<string> jugadasPlayer2 = new List<string>();
+
+            jugadasPlayer1.Add("R");
+            jugadasPlayer2.Add("S");
+
+            jugadasPlayer1.Add("S");
+            jugadasPlayer2.Add("S");
+
+            jugadasPlayer1.Add("P");
+            jugadasPlayer2.Add("S");
+
+            string ganador = ConteoPartidas(jugadasPlayer1, jugadasPlayer2);
+
+            Console.WriteLine("Ganador final: " + ganador);
+
+            Console.ReadKey();
+        }
+
+        public static string ConteoPartidas(List<string> jugadasPlayer1, List<string> jugadasPlayer2)
+        {
+            int puntajePlayer1 = 0;
+            int puntajePlayer2 = 0;
+
+            int posicion = 0;
+
+            foreach (string jugadaPlayer1 in jugadasPlayer1)
+            {
+                string jugadaPlayer2 = jugadasPlayer2[posicion];
+
+                if (jugadaPlayer1 == jugadaPlayer2)
+                {
+                    Console.WriteLine("Empate en la partida " + (posicion + 1));
+                }
+                else if (jugadaPlayer1 == "R" && jugadaPlayer2 == "S")
+                {
+                    Console.WriteLine("Player 1 gana la partida " + (posicion + 1));
+                    puntajePlayer1++;
+                }
+                else if (jugadaPlayer1 == "S" && jugadaPlayer2 == "P")
+                {
+                    Console.WriteLine("Player 1 gana la partida " + (posicion + 1));    
+                    puntajePlayer1++;
+                }
+                else if (jugadaPlayer1 == "P" && jugadaPlayer2 == "R")
+                {
+                    Console.WriteLine("Player 1 gana la partida " + (posicion + 1));
+                    puntajePlayer1++;
+                }
+                else
+                {
+                    Console.WriteLine("Player 2 gana la partida " + (posicion + 1));
+                    puntajePlayer2++;
+                }
+
+                posicion++;
+            }
+
+            return "";
         }
     }
 }
