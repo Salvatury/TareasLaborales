d620a4b3cb85b3240f8c0012ae0b025bd1c6bd32
Salvador Caldarella <s.caldarella82@gmail.com>
2026-06-18 12:14:28 -0300

    orden en el proyecto

---
+++ TareasLaborales/001/Ejercicios/001_CompararVectores.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _001.Ejercicios
+{
+    internal class CompararVectores
+    {
+        public static void Ejecutar()
+        {
+            int[] v1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] v2 = { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+
+            List<int> resultado = CompararVectoresEnteros(v1, v2, false);
+
+            foreach (int numero in resultado)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+
+        static List<int> CompararVectoresEnteros(int[] v1, int[] v2, bool comp)
+        {
+            List<int> resultado = new List<int>();
+
+            foreach (int numeroV1 in v1)
+            {
+                bool existe = false;
+
+                foreach (int numeroV2 in v2)
+                {
+                    if (numeroV1 == numeroV2)
+                    {
+                        existe = true;
+                        break;
+                    }
+                }
+
+                if (comp == true)
+                {
+                    if (existe)
+                    {
+                        resultado.Add(numeroV1);
+                    }
+                }
+                else
+                {
+                    if (!existe)
+                    {
+                        resultado.Add(numeroV1);
+                    }
+                }
+            }
+
+            if (comp == false)
+            {
+                foreach (int numeroV2 in v2)
+                {
+                    bool existe = false;
+
+                    foreach (int numeroV1 in v1)
+                    {
+                        if (numeroV2 == numeroV1)
+                        {
+                            existe = true;
+                            break;
+                        }
+                    }
+
+                    if (!existe)
+                    {
+                        resultado.Add(numeroV2);
+                    }
+                }
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ TareasLaborales/001/Ejercicios/002_PiedraPapelTijera.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _001.Ejercicios
+{
+    internal class PiedraPapelTijera
+    {
+        /*
+         * Crea un programa que calcule quien gana más partidas al piedra,
+         * papel, tijera.
+         * - El resultado puede ser: "Player 1", "Player 2", "Tie" (empate)
+         * - La función recibe un listado que contiene pares, representando cada jugada.
+         * - El par puede contener combinaciones de "R" (piedra), "P" (papel)
+         *   o "S" (tijera).
+         * - Ejemplo. Entrada: [("R","S"), ("S","R"), ("P","S")]. Resultado: "Player 2".
+         */
+        public static void Ejecutar()
+        {
+            List<string> jugadasPlayer1 = new List<string>();
+            List<string> jugadasPlayer2 = new List<string>();
+
+            jugadasPlayer1.Add("R");
+            jugadasPlayer2.Add("P");
+
+            jugadasPlayer1.Add("S");
+            jugadasPlayer2.Add("S");
+
+            jugadasPlayer1.Add("P");
+            jugadasPlayer2.Add("S");
+
+            string ganador = ConteoPartidas(jugadasPlayer1, jugadasPlayer2);
+
+            Console.WriteLine("Ganador: " + ganador);
+        }
+
+        public static string ConteoPartidas(List<string> jugadasPlayer1, List<string> jugadasPlayer2)
+        {
+            int puntajePlayer1 = 0;
+            int puntajePlayer2 = 0;
+
+            for (int i = 0; i < jugadasPlayer1.Count; i++)
+            {
+                string jugadaPlayer1 = jugadasPlayer1[i];
+                string jugadaPlayer2 = jugadasPlayer2[i];
+
+                if (jugadaPlayer1 == jugadaPlayer2)
+                {
+                    Console.WriteLine("Empate en la partida " + (i + 1));
+                }
+                else if (jugadaPlayer1 == "R" && jugadaPlayer2 == "S")
+                {
+                    Console.WriteLine("Player 1 gana la partida " + (i + 1));
+                    puntajePlayer1++;
+                }
+                else if (jugadaPlayer1 == "S" && jugadaPlayer2 == "P")
+                {
+                    Console.WriteLine("Player 1 gana la partida " + (i + 1));
+                    puntajePlayer1++;
+                }
+                else if (jugadaPlayer1 == "P" && jugadaPlayer2 == "R")
+                {
+                    Console.WriteLine("Player 1 gana la partida " + (i + 1));
+                    puntajePlayer1++;
+                }
+                else
+                {
+                    Console.WriteLine("Player 2 gana la partida " + (i + 1));
+                    puntajePlayer2++;
+                }
+            }
+
+            Console.WriteLine("Puntaje Player 1: " + puntajePlayer1);
+            Console.WriteLine("Puntaje Player 2: " + puntajePlayer2);
+
+            if (puntajePlayer1 > puntajePlayer2)
+            {
+                return "Player 1";
+            }
+            else if (puntajePlayer2 > puntajePlayer1)
+            {
+                return "Player 2";
+            }
+            else
+            {
+                return "Tie";
+            }
+        }
+    }
+}
+
