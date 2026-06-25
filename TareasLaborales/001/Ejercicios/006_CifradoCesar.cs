588f9cb5c8911901644abeb5f14d65f04bc341e2
Salvador Caldarella <s.caldarella82@gmail.com>
2026-06-25 13:47:38 -0300

    006 primer commit

---
+++ TareasLaborales/001/Ejercicios/006_CifradoCesar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001.Ejercicios
+{
+    internal class _006_CifradoCesar
+    {
+        /*
+         * Crea un programa que realize el cifrado César de un texto y lo imprima.
+         * También debe ser capaz de descifrarlo cuando así se lo indiquemos.
+         *
+         * Te recomiendo que busques información para conocer en profundidad cómo
+         * realizar el cifrado. Esto también forma parte del reto.
+         */
+
+        public static void Ejecutar()
+        {
+
+        }
+
+
+    }
+}
